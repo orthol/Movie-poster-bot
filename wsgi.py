3f1dace649db53b7a6229f9931496b5b98928eff
orthol <85133459+orthol@users.noreply.github.com>
2025-10-27 23:22:15 +0300

    Change project path and set environment variables
    Updated project path and environment variables for the bot.

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Add your project directory to the Python path
-path = '/home/yourusername/movie-bot'
+path = '/home/orthol/movie-bot'
 if path not in sys.path:
     sys.path.insert(0, path)
 
@@ -10,6 +10,6 @@ if path not in sys.path:
 from app import app as application
 
 # Set environment variables (you can also set these in PythonAnywhere dashboard)
-os.environ['BOT_TOKEN'] = 'your_bot_token_here'
-os.environ['TMDB_API_KEY'] = 'your_tmdb_api_key_here'
+os.environ['BOT_TOKEN'] = '914686489:AAFiBRO_twxrFJexdlpimuOIlAa_qM8oUbE'
+os.environ['TMDB_API_KEY'] = '2197eff09f710270032b79e6ceabdcdb'
 os.environ['PYTHONANYWHERE_URL'] = 'https://yourusername.pythonanywhere.com'
